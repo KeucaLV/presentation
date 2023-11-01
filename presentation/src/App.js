5df6c1924f18cf847c3e08b1907dfa79efba239d
MartinRyannn <139060230+MartinRyannn@users.noreply.github.com>
2023-11-01 14:52:32 +0200

    dasd

--- presentation/src/App.js
+++ presentation/src/App.js
@@ -1,18 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './components/MainPage.js';
-import SlideM from './components/SlideM.js';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/slideM" element={<SlideM />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+<MainPage/>
+    </div>
   );
 }
 
