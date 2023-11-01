1c976cfd1d1c9ae60c646f58ca18f107b4d30d42
MartinRyannn <139060230+MartinRyannn@users.noreply.github.com>
2023-11-01 14:44:58 +0200

    martins

--- presentation/src/App.js
+++ presentation/src/App.js
@@ -1,10 +1,18 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainPage from './components/MainPage.js';
+import SlideM from './components/SlideM.js';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/slideM" element={<SlideM />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ presentation/src/components/MainPage.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './mainStyles.css';
+
+
+function MainPage(){
+    return(
+        <>
+        
+        <div class="container">
+            <div class="temaHeading">USE CASE DIAGRAM</div>
+            <div class="grupaHeading">SMAGIE IMIGRANTI</div>
+            <div class="buttonHeading">
+                <button class="poga">↓</button>
+            </div>
+        </div>
+        
+        </>
+    );
+}
+
+export default MainPage;
