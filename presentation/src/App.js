bb07abcf6b02396a6e44046016db7e098f19aecf
KeucaLV <ipa21.n.markitans@vtdt.edu.lv>
2023-11-01 14:52:17 +0200

    done

--- presentation/src/App.js
+++ presentation/src/App.js
@@ -1,10 +1,15 @@
+import "./App.css";
+import Punkts4 from "./components/4-punkts.js";
+import Punkts5 from "./components/5-punkts.js";
+import "./components/KadIzmanto.css";
 
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <>
+        <Punkts4 />
+          <Punkts5 />
+      </>
   );
 }
 
