45e871b2a24c9c2bec54d670f4c52254218891c7
Reinis Artimovics <124902374+agranoli@users.noreply.github.com>
2023-11-01 15:06:04 +0200

    bv

---
+++ presentation/src/components/Punkts5.js
@@ -0,0 +1,24 @@
+import "./Kad.css";
+import image from "./blogDiagram.drawio.png";
+function Punkts5(){
+    return (
+        <>
+            <div className="mainKad">
+                <div className="titleKad">
+                    <h1>kā šī diagramma izskatās Bloga gadījumā?</h1>
+                </div>
+                <div className="flex-row">
+                    <div className="boxKad">
+                        <div className="textKad">
+                            <h1>Ja jāizveido Use State diagramma saistībā ar mūsu veidoto
+                                Blog lapu tad tā varētu izskatīties šādi</h1>
+                        </div>
+                    </div>
+                    <img alt="diagramaa" className="image" src={image}></img>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Punkts5;
